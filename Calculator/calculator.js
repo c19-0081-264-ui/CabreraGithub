a27ce349f43c9753cf73ecf68860778409127bde
Princess Cabrera <c19-0081-264@uphsl.edu.ph>
2025-10-27 20:50:39 +0800

    Add basic arithmetic functions and example usage

---
+++ Calculator/calculator.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    return a+b;
+}
+
+function subtraction(a, b) {
+    return a-b;
+}
+
+function multiplication(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+let a = 5;
+let b = 3;
+console.log("ADD:", add(a, b));
+console.log("SUBTRACT: ", subtraction(a, b));
+console.log("MULTIPLICATION:", multiplication(a, b));
+console.log("DIVIDE:", divide(a, b));
